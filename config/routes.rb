16e87ed7d844a5eb954de4ec2f29d9bfa9e27d23
yuichi <yuichitao2670@gmail.com>
2022-08-30 18:14:41 +0900

    articles controller に変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'home#index'
-  get '/about' => 'home#about'
+  root to: 'articles#index'
 end
