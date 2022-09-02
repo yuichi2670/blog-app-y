2fa66640ff53d9976f541aa57926377f186e0c14
yuichi <yuichitao2670@gmail.com>
2022-09-02 15:44:36 +0900

    記事を更新できるように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,23 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def edit
+        # urlのIDを取得。viewのarticleに渡す
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        # もし入れたデータを更新されていたら、更新された記事のページに遷移する
+        if @article.update(article_params)
+            redirect_to article_path(@article), notice: '更新できました'
+        # 保存されなければ編集画面に戻る
+        else
+            flash.now[:error] = '更新に失敗しました'
+            render :edit
+        end
+    end
+
     # formから受け取った情報（ここではtitleとcontent）を抜き出す。
     private
     def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
 end
